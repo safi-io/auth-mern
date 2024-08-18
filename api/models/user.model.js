33f38b93e9b1d9b3c16ec4e675796adb491bfe44
safi-io <m.safi.ullah@outlook.com>
2024-08-18 11:04:14 +0500

    Create and Add User model

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.Schema("User", userSchema);
+module.exports = User;
