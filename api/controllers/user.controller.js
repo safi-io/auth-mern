7d9a2fc954b7f521b43c3090bfd27d8a395dfcc0
safi-io <m.safi.ullah@outlook.com>
2024-08-18 11:15:01 +0500

    Create Test API Route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+  return res.send("API IS WORKING");
+};
--- api/index.js
+++ api/index.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import userRoutes from "./routes/user.route.js";
+
 const app = express();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("DB Connected"))
   .catch(() => console.log("DB not Connected"));
+
+app.use("/", userRoutes);
 
 app.listen(3000, () => console.log("Server on 3000"));
