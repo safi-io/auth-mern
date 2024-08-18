c357fa74344033c2c6e203bb2b6857aeaeef5447
safi-io <m.safi.ullah@outlook.com>
2024-08-18 10:59:36 +0500

    Connection with DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("DB Connected"))
+  .catch(() => console.log("DB not Connected"));
 
 app.listen(3000, () => console.log("Server on 3000"));
